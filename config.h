be51d9da35b964b1ab5d4c0b0454f2ca93938e72
elanegan <e.lanegan@lancaster.ac.uk>
2023-01-06 14:20:19 +0000

    Change terminal opening command.

--- config.h
+++ config.h
@@ -65,7 +65,7 @@ static const char *lockcmd[]  = { "slock", NULL };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
     { MODKEY|ControlMask|ShiftMask, XK_l,      spawn,          {.v = lockcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
